71c3bca0a0bbf4f7b5ca23e551e68fc732b9fda6
jhon <jhonsonzabala37@gmail.com>
2022-08-05 23:20:08 -0300

    validacion formulario js

--- js/validacion.js
+++ js/validacion.js
@@ -2,6 +2,22 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("validacion").addEventListener('submit',validarDatos);
 });
-function validarDatos(evento){
+function validarDatos(){
+   
+    var user = document.getElementById('user').value;
+    var pwd = document.getElementById('pwd').value;
+    if(user === "jhon" && pwd === "123456"){
+        
+        alert("Logeo correcto");
+        
+
+    }   
+    else{
+        alert("logeo incorrecto");
+    }
+
+   
+ 
+
   
 }
