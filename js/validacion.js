dd85696cc6749de9b5a65f5986954f4c7ba273f5
jhon <jhonsonzabala37@gmail.com>
2022-08-05 22:18:28 -0300

    first commit

---
+++ js/validacion.js
@@ -0,0 +1,7 @@
+'use strict'
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("validacion").addEventListener('submit',validarDatos);
+});
+function validarDatos(evento){
+  
+}
